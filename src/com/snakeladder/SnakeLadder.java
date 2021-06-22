0102c10150095642be53c85528b8f38d7ec96f49
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 19:39:11 +0530

    Added: RollsDie

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -1,8 +1,9 @@
 package com.snakeladder;
 
 public class SnakeLadder {
-	public static void main(String[] args) {
-		System.out.println("Snake and Ladder game played with single player at start position 0");
-	}
 
+	public static void main(String[] args) {
+		int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+		System.out.println("Output of rolling the die is: " + roll_die);
+	}
 }
