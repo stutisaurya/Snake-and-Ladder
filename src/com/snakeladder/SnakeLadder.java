57bb1ee389af420afd4571924426135346663665
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 19:56:43 +0530

    Added: PlayerWinCase

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -4,31 +4,39 @@ public class SnakeLadder {
 	public static final int noPlay = 0;
 	public static final int ladder = 1;
 	public static final int snake = 2;
+	public static final int win = 100;
 
 	// User check for a option
 	public static void checkOption() {
-		int initial_position = 0;
-		int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
-		int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
-		switch (check_option) {
-		case noPlay:
-			initial_position += 0;
-			System.out.println(initial_position);
-			break;
-		case ladder:
-			initial_position += roll_die;
-			System.out.println(initial_position);
-			break;
-		case snake:
-			initial_position -= roll_die;
-			System.out.println(initial_position);
-			break;
-		}
 
+		int initial_position = 0;
+		while (initial_position < win) {
+			int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+			int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
+			switch (check_option) {
+			case noPlay:
+				initial_position += 0;
+				System.out.println(initial_position);
+				break;
+			case ladder:
+				initial_position += roll_die;
+				System.out.println(initial_position);
+				break;
+			case snake:
+				initial_position -= roll_die;
+				if (initial_position < 0) {
+					initial_position = 0;
+					System.out.println(initial_position);
+				} else {
+					System.out.println(initial_position);
+				}
+				break;
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
 		checkOption();
 	}
-
 }
