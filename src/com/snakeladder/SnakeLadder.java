6b42d2a5944e60f0129f18ad487ed2d9f700e23c
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 19:33:54 +0530

    Added: startPosition

---
+++ src/com/snakeladder/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.snakeladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		System.out.println("Snake and Ladder game played with single player at start position 0");
+	}
+
+}
