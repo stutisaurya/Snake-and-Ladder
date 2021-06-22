1d3a442db5f60da2dd4e8da4e0f2f60fe3fecdfc
stutisaurya <stuti.saurya@gmail.com>
2021-06-22 20:09:10 +0530

    Added: TwoPlayerGame

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -6,42 +6,76 @@ public class SnakeLadder {
 	public static final int snake = 2;
 	public static final int win = 100;
 
-	// User check for a option
+	// Two player game
 	public static void checkOption() {
 
-		int count = 0;
-		int initial_position = 0;
-		while (initial_position < win) {
+		int initial_position_player1 = 0;
+		int initial_position_player2 = 0;
+
+		while ((initial_position_player1 < win) || (initial_position_player2 < win)) {
 			int roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
 			int check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
 			switch (check_option) {
 			case noPlay:
-				initial_position += 0;
-				System.out.println(initial_position);
+				initial_position_player1 += 0;
+				System.out.println("Position of Player 1- " + initial_position_player1);
 				break;
 			case ladder:
-				initial_position += roll_die;
-				if (initial_position > 100) {
-					initial_position -= roll_die;
-					System.out.println(initial_position);
+				initial_position_player1 += roll_die;
+				if (initial_position_player1 > 100) {
+					initial_position_player1 -= roll_die;
+					System.out.println("Position of Player 1- " + initial_position_player1);
 				} else {
-					System.out.println(initial_position);
+					System.out.println("Position of Player 1- " + initial_position_player1);
+					if (initial_position_player1 == 100) {
+						System.out.println("Player one wins");
+						System.exit(1);
+					}
 				}
 				break;
 			case snake:
-				initial_position -= roll_die;
-				if (initial_position < 0) {
-					initial_position = 0;
-					System.out.println(initial_position);
+				initial_position_player1 -= roll_die;
+				if (initial_position_player1 < 0) {
+					initial_position_player1 = 0;
+					System.out.println("Position of Player 1- " + initial_position_player1);
 				} else {
-					System.out.println(initial_position);
+					System.out.println("Position of Player 1- " + initial_position_player1);
 				}
 				break;
 			}
-			count++;
+
+			roll_die = (int) ((Math.floor(Math.random() * 10)) % 6) + 1;
+			check_option = (int) ((Math.floor(Math.random() * 10)) % 3);
+			switch (check_option) {
+			case noPlay:
+				initial_position_player2 += 0;
+				System.out.println("Position of Player 2- " + initial_position_player2);
+				break;
+			case ladder:
+				initial_position_player2 += roll_die;
+				if (initial_position_player2 > 100) {
+					initial_position_player2 -= roll_die;
+					System.out.println("Position of Player 2- " + initial_position_player2);
+				} else {
+					System.out.println("Position of Player 2- " + initial_position_player2);
+					if (initial_position_player2 == 100) {
+						System.out.println("Player two wins");
+						System.exit(1);
+					}
+				}
+				break;
+			case snake:
+				initial_position_player2 -= roll_die;
+				if (initial_position_player2 < 0) {
+					initial_position_player2 = 0;
+					System.out.println("Position of Player 2- " + initial_position_player2);
+				} else {
+					System.out.println("Position of Player 2- " + initial_position_player2);
+				}
+				break;
+			}
 		}
 
-		System.out.println("Number of times dice was played to win the game is " + count);
 	}
 
 	public static void main(String[] args) {
